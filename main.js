90121f5c7f7222fc355b82e15c042da6a53e8cd7
Chen Li <chenli@Chens-MacBook-Pro.local>
2020-09-09 00:33:31 -0400

    class one, toy react

---
+++ main.js
@@ -0,0 +1,20 @@
+import {createElement, Component, render} from './toy-react';
+
+class MyComponent extends Component {
+    render(){
+        return <div>
+            <h1>My Component</h1>
+            {this.children}
+        </div>
+    }
+}
+
+
+
+render(
+<MyComponent id="a" class= "c">
+    <div>abc</div>
+    <div></div>
+    <div></div>
+    </MyComponent>, document.body
+)
